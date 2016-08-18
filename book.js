6accacd7692fefd8ea39d085f5b5d49b57264087
Kevin Turner <kevin@ksturner.com>
2016-08-17 23:07:17 -0500

    initial commit

---
+++ book.js
@@ -0,0 +1,138 @@
+
+
+export class Bible {
+    constructor() {
+        this.books = []
+    }
+
+    addBook(book) {
+        this.books.push(book);
+    }
+
+    haveBookName(bookName) {
+        const filteredBooks = this.books.filter( (b) => {
+            return b._name === bookName;
+        });
+        return filteredBooks.length > 0;
+    }
+    getBookByName(bookName) {
+        // TODO: need a way to filter by shortname, longname, etc. too...
+        const filteredBooks = this.books.filter( (b) => {
+            return b._name === bookName;
+        });
+        return filteredBooks.length ? filteredBooks[0] : null;
+    }
+    toJSON() {
+        return {
+            books: this.books.map( (b) => {
+                return b.toJSON();
+            }),
+        };
+    }
+}
+
+export class BibleBook {
+    constructor(name, shortName, longName) {
+        this._name =  name;
+        this._shortName = shortName;
+        this._longName = longName;
+        this.chapters = [];
+    }
+    set shortName(name) {
+        this._shortName = name;
+    }
+    set name(name) {
+        this._name = name;
+    }
+    set longName(name) {
+        this._longName = name;
+    }
+    addChapter(chapter) {
+        this.chapters.push(chapter);
+    }
+
+    haveChapterNumber(chapterNumber) {
+        return getChapterByNumber(chapterNumber) !== null;
+    }
+    getChapterByNumber(chapterNumber) {
+        const filteredChapters = this.chapters.filter( (c) => {
+            return c.number === chapterNumber;
+        });
+        return filteredChapters.length ? filteredChapters[0] : null;
+    }
+
+    toJSON() {
+        return {
+            name: this._name,
+            shortName: this._shortName,
+            longName: this._longName,
+            chapters: this.chapters.map( (c) => {
+                return c.toJSON();
+            }),
+        };
+    }
+}
+
+export class BibleChapter {
+    constructor(book, chapterNumber) {
+        this.book = book;
+        this.number = chapterNumber;
+        this.verses = [];
+    }
+    addVerse(verse) {
+        this.verses.push(verse);
+    }
+
+    haveVerseNumber(verseNumber) {
+        return this.getVerseByNumber(verseNumber) !== null;
+    }
+    getVerseByNumber(verseNumber) {
+        const filteredVerses = this.verses.filter( (v) => {
+            return v.number === verseName;
+        });
+        return filteredVerse.length ? filteredVerses[0] : null;
+    }
+
+    toJSON() {
+        return {
+            number: this.number,
+            verses: this.verses.map( (v) => {
+                return v.toJSON2();
+            }),
+        };
+    }
+}
+
+export class BibleVerse {
+
+    /* Need to strive for this format:
+     *
+     * [{"bookname":"John","chapter":"3","verse":"16","text":"For this is the
+     * way God loved the world: He gave his one and only Son, so that everyone
+     * who believes in him will not perish but have eternal life."}]
+     *
+     * because that is the format that the NET bible is using here:
+     *
+     * http://labs.bible.org/api/?passage=John%203:16&type=json
+     */
+    constructor(book, chapter, verseNumber, text) {
+        this.book = book;
+        this.chapter = chapter;
+        this.number = verseNumber;
+        this.text = text;
+    }
+    toJSON() {
+        return {
+            bookname: this.book._shortName,
+            chapter: this.chapter.number,
+            verse: this.number,
+            text: this.text,
+        };
+    }
+    toJSON2() {
+        return {
+            verse: this.number,
+            text: this.text,
+        };
+    }
+}
