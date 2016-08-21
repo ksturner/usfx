3ab92e4a10e8431ee3b0232bad232ec0921067ee
Kevin Turner <kevin@foreflight.com>
2016-08-21 16:46:33 -0500

    adding placeholder test file

---
+++ test/test_parser.js
@@ -0,0 +1,10 @@
+import assert from 'assert';
+import usfx from '../src/usfx';
+
+
+describe('Test USFX Parser', () => {
+    it('can call assert', () => {
+        let x = 1;
+        assert(x);
+    });
+});
