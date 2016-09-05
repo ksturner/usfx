381e1615d6ad9dba2c936c5ba48e8ced8d481056
Kevin Turner <kevin@ksturner.com>
2016-09-05 11:49:50 -0500

    change test desc

--- test/test_parser.js
+++ test/test_parser.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 
 describe('Test USFX Parser', function() {
-    it('can call assert', function() {
+    it('can parse asv', function() {
         this.timeout(5000);
         assert(parseUSFXFile !== undefined, 'usfx import broken; is undefined');
         const dataPath = path.resolve(__dirname, 'asv.xml');
