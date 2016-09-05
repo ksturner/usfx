56862d62ad90a8be144e2d5b50b30f0d52f927a9
Kevin Turner <kevin@ksturner.com>
2016-09-05 11:48:59 -0500

    test case for parser added

--- test/test_parser.js
+++ test/test_parser.js
@@ -1,10 +1,14 @@
 import assert from 'assert';
-import usfx from '../src/usfx';
+import {parseUSFXFile} from '../src/usfx';
+import path from 'path';
 
 
-describe('Test USFX Parser', () => {
-    it('can call assert', () => {
-        let x = 1;
-        assert(x);
+describe('Test USFX Parser', function() {
+    it('can call assert', function() {
+        this.timeout(5000);
+        assert(parseUSFXFile !== undefined, 'usfx import broken; is undefined');
+        const dataPath = path.resolve(__dirname, 'asv.xml');
+        const bible = parseUSFXFile(path.resolve(__dirname, 'asv.xml'));
+        assert(bible.books.length === 66);
     });
 });
